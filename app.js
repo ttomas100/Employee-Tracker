881a2f7ad783042927181de05beecf40cd6ce39d
Tomas Utreras <tutreras100@gmail.com>
2021-01-01 22:05:33 -0500

    inquirer and get DB

--- app.js
+++ app.js
@@ -24,3 +24,66 @@ var connection = mysql.createConnection({
     getManagers();
     getEmployees();
   });
+
+  start = () => {
+
+    inquirer
+      .prompt({
+        name: "choices",
+        type: "list",
+        message: "Please choose one of the following options:",
+        choices: ["ADD", "VIEW", "UPDATE", "DELETE", "EXIT"]
+      })
+      .then(function(answer) {
+        if (answer.choices === "ADD") {
+          addSomething();
+        }
+        else if (answer.choices === "VIEW") {
+          viewSomething();
+        } 
+        else if (answer.choices === "UPDATE") {
+          updateSomething();
+        }
+        else if (answer.choices === "DELETE") {
+          deleteSomething();
+        }
+        else if (answer.choices === "EXIT") {  
+          connection.end();
+        }
+        else{
+          connection.end();
+        }
+      });
+  }
+
+getRoles = () => {
+  connection.query("SELECT id, title FROM role", (err, res) => {
+    if (err) throw err;
+    roles = res;
+  
+  })
+};
+
+getDepartments = () => {
+  connection.query("SELECT id, name FROM department", (err, res) => {
+    if (err) throw err;
+    departments = res;
+
+  })
+};
+
+getManagers = () => {
+  connection.query("SELECT id, first_name, last_name, CONCAT_WS(' ', first_name, last_name) AS managers FROM employee", (err, res) => {
+    if (err) throw err;
+    managers = res;
+
+  })
+};
+
+getEmployees = () => {
+  connection.query("SELECT id, CONCAT_WS(' ', first_name, last_name) AS Employee_Name FROM employee", (err, res) => {
+    if (err) throw err;
+    employees = res;
+
+  })
+};
